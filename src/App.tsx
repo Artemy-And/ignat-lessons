a6887010c01f18456fdaf9fc1200d24d708fdf6f
Artemy-And <andruschak.artemy@gmail.com>
2020-06-03 13:43:25 +0300

    added folder state and oprokinul prosps to and changed app file

--- src/App.tsx
+++ src/App.tsx
@@ -14,40 +14,9 @@ import {
   dialogsDataType,
   dialogsMessagesDataType,
 } from "./components/Dialogs/Dialogs";
+import state from "./redux/state";
 
 const App = () => {
-  let posts: Array<postsType> = [
-    { id: 1, message: "Hi, how are you?", countLikes: 10 },
-    { id: 2, message: "Hi, how are you?", countLikes: 133 },
-    { id: 3, message: "Hi, how are you?", countLikes: 4432 },
-  ];
-
-  let dialogs: Array<dialogsDataType> = [
-    { id: 1, name: "Artemy" },
-    { id: 2, name: "Alevtina" },
-    { id: 3, name: "Akseniya" },
-    { id: 4, name: "Afonasii" },
-    { id: 5, name: "Belanton" },
-    { id: 6, name: "Stephan" },
-    { id: 7, name: "Stephan" },
-    { id: 8, name: "Stephan" },
-    { id: 9, name: "Stephan" },
-  ];
-  let messages: Array<dialogsMessagesDataType> = [
-    { id: 1, message: "Halllooo?" },
-    { id: 2, message: "Hey how are you?" },
-    { id: 3, message: "Hey how are you?" },
-    { id: 4, message: "Hey Buddy" },
-    { id: 5, message: "Hey how are you?" },
-    { id: 6, message: "Hey how are you?" },
-    { id: 7, message: "Hey how are you?" },
-    { id: 8, message: "Hey how are you?" },
-    { id: 9, message: "Hey how are you?" },
-    { id: 10, message: "Hey Buddy" },
-    { id: 11, message: "Hey how are you?" },
-    { id: 12, message: "Hey how are you?" },
-  ];
-
   return (
     <BrowserRouter>
       <div className="app-wrapper">
@@ -59,13 +28,15 @@ const App = () => {
             exact
             path="/dialogs"
             component={() => {
-              return <Dialogs dialogs={dialogs} messages={messages} />;
+              return (
+                <Dialogs dialogs={state.dialogs} messages={state.messages} />
+              );
             }}
           />
           <Route
             path="/profile"
             component={() => {
-              return <Profile posts={posts} />;
+              return <Profile posts={state.posts} />;
             }}
           />
           <Route path="/news" component={News} />
@@ -78,3 +49,35 @@ const App = () => {
 };
 
 export default App;
+
+// let posts: Array<postsType> = [
+//   { id: 1, message: "Hi, how are you?", countLikes: 10 },
+//   { id: 2, message: "Hi, how are you?", countLikes: 133 },
+//   { id: 3, message: "Hi, how are you?", countLikes: 4432 },
+// ];
+
+// let dialogs: Array<dialogsDataType> = [
+//   { id: 1, name: "Artemy" },
+//   { id: 2, name: "Alevtina" },
+//   { id: 3, name: "Akseniya" },
+//   { id: 4, name: "Afonasii" },
+//   { id: 5, name: "Belanton" },
+//   { id: 6, name: "Stephan" },
+//   { id: 7, name: "Stephan" },
+//   { id: 8, name: "Stephan" },
+//   { id: 9, name: "Stephan" },
+// ];
+// let messages: Array<dialogsMessagesDataType> = [
+//   { id: 1, message: "Halllooo?" },
+//   { id: 2, message: "Hey how are you?" },
+//   { id: 3, message: "Hey how are you?" },
+//   { id: 4, message: "Hey Buddy" },
+//   { id: 5, message: "Hey how are you?" },
+//   { id: 6, message: "Hey how are you?" },
+//   { id: 7, message: "Hey how are you?" },
+//   { id: 8, message: "Hey how are you?" },
+//   { id: 9, message: "Hey how are you?" },
+//   { id: 10, message: "Hey Buddy" },
+//   { id: 11, message: "Hey how are you?" },
+//   { id: 12, message: "Hey how are you?" },
+// ];
