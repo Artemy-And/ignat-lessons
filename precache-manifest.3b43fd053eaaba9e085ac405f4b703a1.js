a55c8b2f1bbcc98f4c10ac792de867b5bdc82ff2
Artemy-And <andruschak.artemy@gmail.com>
2020-09-28 16:57:02 +0300

    Updates

---
+++ precache-manifest.3b43fd053eaaba9e085ac405f4b703a1.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23bd20238d39f84bf8f6bf168aa58d51",
+    "url": "/logistics-social-network/index.html"
+  },
+  {
+    "revision": "784d1d1939d517cb21e1",
+    "url": "/logistics-social-network/static/css/3.bcd8879f.chunk.css"
+  },
+  {
+    "revision": "d7e63fd1e7aab3f7ac0a",
+    "url": "/logistics-social-network/static/css/4.9950c3a6.chunk.css"
+  },
+  {
+    "revision": "399a956c0994f0ea4d91",
+    "url": "/logistics-social-network/static/css/main.4e547475.chunk.css"
+  },
+  {
+    "revision": "769f4e5063c49dfd805a",
+    "url": "/logistics-social-network/static/js/2.1c649c72.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/logistics-social-network/static/js/2.1c649c72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "784d1d1939d517cb21e1",
+    "url": "/logistics-social-network/static/js/3.f206e492.chunk.js"
+  },
+  {
+    "revision": "d7e63fd1e7aab3f7ac0a",
+    "url": "/logistics-social-network/static/js/4.cd256ed1.chunk.js"
+  },
+  {
+    "revision": "399a956c0994f0ea4d91",
+    "url": "/logistics-social-network/static/js/main.b76e210a.chunk.js"
+  },
+  {
+    "revision": "916ab50ad985513a0ab4",
+    "url": "/logistics-social-network/static/js/runtime-main.1dfdb625.js"
+  },
+  {
+    "revision": "b3e90dfeb6b3b2de52eddd2c94ab19ab",
+    "url": "/logistics-social-network/static/media/iconmen.b3e90dfe.png"
+  },
+  {
+    "revision": "79c69891b574806931ece74544268495",
+    "url": "/logistics-social-network/static/media/loader.79c69891.svg"
+  }
+]);
